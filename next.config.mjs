17db0244ea375fd4a7513f52c23f5470f81451b7
Helder Fonseca <helder2391@gmail.com>
2024-03-20 14:21:52 -0100

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['react-bootstrap'],
+  },
+};
 
 export default nextConfig;
