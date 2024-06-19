424e59a6bb2aa4a8be2d17b522101478a2ef7f5a
Helder Fonseca <helder2391@gmail.com>
2024-06-19 18:59:45 -0100

    cmit

--- src/app/components/contact-form/contactForm.tsx
+++ src/app/components/contact-form/contactForm.tsx
@@ -6,8 +6,40 @@ import Col from 'react-bootstrap/Col';
 import "./contactForm.css";
 import { Form, Button } from 'react-bootstrap';
 import * as Icon from 'react-bootstrap-icons';
+import { useEffect, useRef, useState } from 'react';
 
 function ContactForm() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [subject, setSubject] = useState('');
+
+    const data = {
+        name,
+        email,
+        phone,
+        subject
+    }
+
+    useEffect(() => {
+        fetch('https://formsubmit.co/digitechitcv@gmail.com', {
+        method: 'POST',
+            body: data,
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error));
+    },[])
+
+    const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // const data = new FormData(form.current);
+    
+    console.dir(data);
+
+    
+    };
     return (
         <Container fluid className='containerForm '>
             <Container className='content-container'>
@@ -19,32 +51,32 @@ function ContactForm() {
                         <p> <Icon.CheckCircleFill/> Temos a reposta para si</p>
                     </Col>
                     <Col className='right-side1'>
-                        <Form>
+                        <Form method="POST" encType="multipart/form-data" onSubmit={handleSubmit}>
                             <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridName">
                                 <Form.Label>Nome</Form.Label>
-                                <Form.Control type="text" placeholder="Insira Nome" />
+                                <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Insira Nome" />
                                 </Form.Group>
 
-                                 <Form.Group as={Col} controlId="formGridEmail">
+                                <Form.Group as={Col} controlId="formGridEmail">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Insira email" />
+                                <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Insira email" />
                                 </Form.Group>
                             </Row>
                             <Row className="mb-3">
                                 <Form.Group as={Col} controlId="formGridTel">
                                 <Form.Label>Numero</Form.Label>
-                                <Form.Control type="tel" placeholder="Insira seu numero" />
+                                <Form.Control type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} placeholder="Insira seu numero" />
                                 </Form.Group>
 
                                 <Form.Group as={Col} controlId="formGridSelect">
                                 <Form.Label>Assunto</Form.Label>
-                                <Form.Select aria-label="Escola a sua opção">
+                                <Form.Select aria-label="Escola a sua opção" value={subject} onChange={(e) => setSubject(e.target.value)}>
                                 <option>Escola a sua opção</option>
-                                <option value="1">Desenvolvimento de Software</option>
-                                <option value="2">Assistência Técnica</option>
-                                <option value="3">Formação em TICS </option>
-                                <option value="4">Marketing Digital</option>
+                                <option value="Desenvolvimento de Software">Desenvolvimento de Software</option>
+                                <option value="Assistência Técnica">Assistência Técnica</option>
+                                <option value="Formação em TICS">Formação em TICS </option>
+                                <option value="Marketing Digital">Marketing Digital</option>
                                 </Form.Select>
                                 </Form.Group>
                                 
@@ -56,8 +88,8 @@ function ContactForm() {
                     </Col>
                 </Row>
             </Container>    
-      </Container>
+    </Container>
     );
-  }
-  
-  export default ContactForm;
+}
+
+export default ContactForm;
