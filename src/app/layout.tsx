0451b8dd605c4095e278047a1b4f85ef607cd681
Jelson Tavares <jelsonvarelatavares@gmail.com>
2025-07-08 21:09:38 +0200

    fix: corrigir paths case-sensitive para deploy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import GoogleAnalytics from "./components/google-analytics/GoogleAnalytics";
-import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Digitechcv | Lda",
